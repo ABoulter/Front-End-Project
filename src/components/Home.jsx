ae4615f580c9cba02b3d8adcb0be954087248a57
Exo <paink37@hotmail.com>
2023-02-08 22:03:57 +0000

    Update de layout da Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,17 +2,21 @@ export default function Home() {
   return (
     <main className="herosection">
       <img src="https://imgur.com/p6i1XFn.png" alt="titulo da pagina" />
-      <section className="shopsection">
-        <ul>
-          <li>
-            <img src="https://imgur.com/rKXn5tR.gif" alt="nova colecção" />
-          </li>
-          <li></li>
-          <li></li>
-        </ul>
-      </section>
       <section>
-        <div></div>
+        <div>
+          <img src="https://imgur.com/rKXn5tR.gif" alt="nova colecção" />
+        </div>
+        <div>
+          <img src="https://imgur.com/UyIdFTh.png" alt="todas as obras" />
+        </div>
+        <div>
+          <img src="https://imgur.com/z2jp7yo.png" alt="todas as obras" />
+        </div>
+      </section>
+      <section className="memegenerator">
+        <div>
+          <img src="https://imgur.com/Y4XZME9.png" alt="meme generator" />
+        </div>
       </section>
     </main>
   );

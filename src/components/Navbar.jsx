c63a991ebc93fe5b786c491d2617347dbc8288d3
Exo <paink37@hotmail.com>
2023-02-08 11:04:06 +0000

    Estilização da navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,24 +3,50 @@ import { NavLink } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="nav">
-      <NavLink to="/">
-        <img
-          src="https://www.simpleimageresizer.com/_uploads/photos/476783fc/Pixelated_Pepe_Sad_Frog_Meme_Rare_Dank_1_80x80.png"
-          alt="Pepe triste"
-        />
-      </NavLink>
       <ul>
-        <li className="active">
-          <NavLink to="/loja">Loja</NavLink>
+        <li>
+          <NavLink to="/loja">
+            <button type="button" className="loja">
+              Loja
+            </button>
+          </NavLink>
         </li>
-        <li className="active">
-          <NavLink to="/sobrenos">sobre nós</NavLink>
+        <li>
+          <NavLink to="/novidades">
+            <button type="button" className="novidades">
+              Novidades
+            </button>
+          </NavLink>
         </li>
-        <li className="active">
-          <NavLink to="/faq">faq</NavLink>
+        <li>
+          <NavLink to="/">
+            <img
+              src="https://www.simpleimageresizer.com/_uploads/photos/476783fc/Pixelated_Pepe_Sad_Frog_Meme_Rare_Dank_1_80x80.png"
+              alt="Pepe triste"
+            />
+          </NavLink>
         </li>
-        <li className="active">
-          <NavLink to="/cart">cart</NavLink>
+
+        <li>
+          <NavLink to="/sobrenos">
+            <button type="button" className="sobrenos">
+              Sobre nós
+            </button>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/faq">
+            <button type="button" className="faq">
+              FAQ
+            </button>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/cart">
+            <button type="button" className="cart">
+              CART
+            </button>
+          </NavLink>
         </li>
       </ul>
     </nav>

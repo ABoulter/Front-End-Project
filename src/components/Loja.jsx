17ec4b5108832bb01211b320c908cc606a0e62ac
Exo <paink37@hotmail.com>
2023-02-14 22:09:05 +0000

    Implementação de modal e ajustes de CSS na Pagina de Loja

--- src/components/Loja.jsx
+++ src/components/Loja.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 
 export default function Loja() {
   const [art, setArt] = useState([]);
+  const [model, setModel] = useState(false);
+  const [tempImgSrc, setTempImgSrc] = useState("");
 
   useEffect(() => {
     fetch("/api/galeria.json")
@@ -9,22 +11,39 @@ export default function Loja() {
       .then((data) => setArt(data));
   }, []);
 
+  const getImg = (image) => {
+    setTempImgSrc(image);
+    setModel(true);
+  };
+
   return (
     <main id="art">
+      <div className={model ? "model open" : "model"}>
+        <img
+          className="closeModal"
+          src="/images/close.png"
+          alt="butão de fechar"
+          onClick={() => setModel(false)}
+        />
+        <img src={tempImgSrc} alt="" />
+      </div>
       {art.map((category) => (
         <section key={category.category_id}>
           <h2>
             <img src={category.category} />
           </h2>
-          <div className={`artList${category.category_id}`}>
+
+          <div id="allList" className={`artList${category.category_id}`}>
             {category.art.map((art) => (
               <div key={art.art_id}>
                 <div>
                   <h3>{art.name}</h3>
-                  <img src={art.image} alt="" />
-                  <div className="hideArt">
-                    <img src={art.image} alt="" />
-                  </div>
+                  <img
+                    src={art.image}
+                    alt=""
+                    onClick={() => getImg(art.image)}
+                  />
+
                   <button type="button">Encomendar!</button>
                 </div>
                 <p>
